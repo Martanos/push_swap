50389ccb24f8fd60e90fefd21268667e309a1469
martanos <martinleex7568@hotmail.com>
2024-05-14 16:27:53 +0800

    Can pass at 85 marks

--- srcs/price_checker_utils.c
+++ srcs/price_checker_utils.c
@@ -6,7 +6,7 @@
 /*   By: malee <malee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 18:41:39 by malee             #+#    #+#             */
-/*   Updated: 2024/05/11 02:17:17 by malee            ###   ########.fr       */
+/*   Updated: 2024/05/14 15:16:19 by malee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ static void	set_target_node(t_node *current_a, t_stack **stack_b)
 			current_b = (*stack_b)->stack_head;
 			while (current_b)
 			{
-				if (current_b->value >= current_a->target_node->value
-					&& current_a->value >= current_b->value)
+				if (current_a->target_node->value < current_b->value
+					&& current_a->value > current_b->value)
 					current_a->target_node = current_b;
 				current_b = current_b->next;
 			}
@@ -36,23 +36,28 @@ static void	set_target_node(t_node *current_a, t_stack **stack_b)
 	}
 }
 
-static void	set_price(t_node *stack_a_head, t_stack **stack_a,
-		t_stack **stack_b)
+static void	set_price(t_stack **stack_a, t_stack **stack_b)
 {
+	t_node	*stack_a_head;
+
+	stack_a_head = (*stack_a)->stack_head;
 	while (stack_a_head)
 	{
-		if (stack_a_head->position >= (*stack_a)->length / 2)
-			stack_a_head->push_price = (*stack_a)->length
-				- stack_a_head->position;
+		if (stack_a_head->position <= (*stack_a)->length / 2)
+			stack_a_head->push_price = stack_a_head->position;
 		else
-			stack_a_head->push_price = ((*stack_a)->length / 2)
-				- stack_a_head->position;
-		if (stack_a_head->target_node->position >= (*stack_b)->length / 2)
-			stack_a_head->push_price += (*stack_b)->length
-				- stack_a_head->target_node->position;
+			stack_a_head->push_price = ((*stack_a)->length
+					- stack_a_head->position) + 1;
+		stack_a_head = stack_a_head->next;
+	}
+	stack_a_head = (*stack_a)->stack_head;
+	while (stack_a_head)
+	{
+		if (stack_a_head->target_node->position <= (*stack_b)->length / 2)
+			stack_a_head->push_price += stack_a_head->target_node->position;
 		else
-			stack_a_head->push_price += ((*stack_b)->length / 2)
-				- stack_a_head->target_node->position;
+			stack_a_head->push_price += ((*stack_b)->length
+					- stack_a_head->target_node->position) + 1;
 		stack_a_head = stack_a_head->next;
 	}
 }
@@ -82,6 +87,6 @@ static void	set_cheapest(t_node *stack_a_head)
 void	set_prices(t_stack **stack_a, t_stack **stack_b)
 {
 	set_target_node((*stack_a)->stack_head, stack_b);
-	set_price((*stack_a)->stack_head, stack_a, stack_b);
+	set_price(stack_a, stack_b);
 	set_cheapest((*stack_a)->stack_head);
 }
--- srcs/sort_utils.c
+++ srcs/sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: malee <malee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 01:25:41 by malee             #+#    #+#             */
-/*   Updated: 2024/05/11 02:46:55 by malee            ###   ########.fr       */
+/*   Updated: 2024/05/14 16:23:58 by malee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ static void	bring_largest_to_top(t_stack **stack)
 {
 	while ((*stack)->max_value != (*stack)->stack_head)
 	{
-		if ((*stack)->max_value->position >= (*stack)->length / 2)
-			rrb(stack);
-		else
+		if ((*stack)->max_value->position <= (*stack)->length / 2)
 			rb(stack);
+		else
+			rrb(stack);
 	}
 }
 
@@ -32,19 +32,6 @@ static void	initial_push(t_stack **stack_a, t_stack **stack_b)
 
 static void	parser(t_node *current, t_stack **stack_a, t_stack **stack_b)
 {
-	// if ((*stack_a)->length > 2)
-	// {
-	// 	if (current != (*stack_a)->stack_head
-	// 		&& current->target_node != (*stack_b)->stack_head)
-	// 	{
-	// 		if (current->position <= (*stack_a)->length / 2
-	// 			&& current->target_node->position <= (*stack_b)->length / 2)
-	// 			rr(stack_a, stack_b);
-	// 		else if (current->position >= (*stack_a)->length / 2
-	// 			&& current->target_node->position >= (*stack_b)->length / 2)
-	// 			rrr(stack_a, stack_b);
-	// 	}
-	// }
 	if (current != (*stack_a)->stack_head)
 	{
 		if (current->position <= (*stack_a)->length / 2)
